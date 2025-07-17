6d7b9c604c9ee3873c53f462a559525cfcc61a38
H-JuKyung <hjk000hjk17@gmail.com>
2025-07-17 14:54:50 +0900

    [VT2-143] refactor: cn 유틸 함수를 utils/cn.ts로 구조 변경

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { cn } from '../../lib/utils'
+import { cn } from '../../utils/cn'
 import { badgeVariants, type BadgeVariantProps } from './badgeVariants'
 
 interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, BadgeVariantProps {
