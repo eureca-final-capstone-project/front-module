621cd2a6c2d1a81c468fc33c46b35d85b1dc7d55
nue-os <kimsoeun0108@gmail.com>
2025-07-17 16:07:36 +0900

    [VT2-155] feat: 로그인 시, 로그인 폼 유효성 검사

--- src/pages/LoginPage/components/LoginForm.tsx
+++ src/pages/LoginPage/components/LoginForm.tsx
@@ -1,17 +1,41 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Button from '../../../components/Button/Button'
 import FloatingLabelInput from '../../../components/FloatingLabelInput/FloatingLabelInput'
+import { validateEmail } from '../../../utils/validation'
 
 const LoginForm = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errors, setErrors] = useState<{ email?: string; password?: string }>({})
+
+  const validateLoginForm = () => {
+    const errors: { email?: string; password?: string } = {}
+
+    const emailError = validateEmail(email)
+    console.log(emailError)
+    if (emailError) errors.email = emailError
+
+    return errors
+  }
+
+  const handleLogin = (e: FormEvent) => {
+    e.preventDefault()
+
+    const validationErrors = validateLoginForm()
+    setErrors(validationErrors)
+  }
+
   return (
-    <form className="flex flex-col gap-10">
+    <form onSubmit={handleLogin} className="flex flex-col gap-10">
       <FloatingLabelInput
         label="이메일"
         id="email"
         value={email}
-        onChange={e => setEmail(e.target.value)}
+        error={errors.email}
+        onChange={e => {
+          setEmail(e.target.value)
+          if (errors.email) setErrors(prev => ({ ...prev, email: undefined }))
+        }}
       />
       <FloatingLabelInput
         label="비밀번호"
@@ -21,6 +45,7 @@ const LoginForm = () => {
       />
       <Button
         text="로그인"
+        type="submit"
         disabled={!email || !password}
         className={email && password ? 'bg-pri-500 text-gray-10' : 'bg-gray-50 text-gray-500'}
       />
