ceb9d5127dc088ad5aea1da1344887bce2203a9b
yeji424 <yejiborn424@gmail.com>
2025-07-20 19:56:50 +0900

    [VT2-141] chore: 이벤트 쿠폰 더미데이터 mock 폴더로 이동

--- src/pages/MyPage/EventCouponPage.tsx
+++ src/pages/MyPage/EventCouponPage.tsx
@@ -1,46 +1,7 @@
 import EventCoupon from './components/EventCoupon/EventCoupon'
-import { eventCouponItem } from '../../apis/eventCoupon'
+import { dummyCoupons } from '../../mocks/EventCouponData'
 import { useDeviceType } from '../../hooks/useDeviceType'
 // import { useQuery } from '@tanstack/react-query'
-
-const dummyCoupons: eventCouponItem[] = [
-  {
-    userEventCouponId: 1,
-    expiresAt: '2025-08-31T23:59:59Z',
-    status: { statusId: 1, code: 'active' },
-    eventCoupon: {
-      eventCouponId: 101,
-      couponNumber: 'CPN001',
-      couponName: '카카오페이 할인',
-      discountRate: 5,
-      payType: { payTypeId: 1, name: '카카오페이' },
-    },
-  },
-  {
-    userEventCouponId: 2,
-    expiresAt: '2025-09-15T23:59:59Z',
-    status: { statusId: 1, code: 'active' },
-    eventCoupon: {
-      eventCouponId: 102,
-      couponNumber: 'CPN002',
-      couponName: '토스페이 캐시백',
-      discountRate: 3,
-      payType: { payTypeId: 2, name: '토스페이' },
-    },
-  },
-  {
-    userEventCouponId: 3,
-    expiresAt: '2025-10-01T23:59:59Z',
-    status: { statusId: 1, code: 'active' },
-    eventCoupon: {
-      eventCouponId: 103,
-      couponNumber: 'CPN003',
-      couponName: '네이버페이 적립',
-      discountRate: 7,
-      payType: { payTypeId: 3, name: '네이버페이' },
-    },
-  },
-]
 
 const EventCouponPage = () => {
   // const { data, isLoading, isError } = useQuery<eventCouponItem[]>({
