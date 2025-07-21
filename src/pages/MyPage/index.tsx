75e918686c60b8c5696e2fe37b169ed1c99e12ba
yeji424 <yejiborn424@gmail.com>
2025-07-21 19:54:23 +0900

    [VT2-240] feat: 마이페이지 상단 레이아웃 구현

--- src/pages/MyPage/index.tsx
+++ src/pages/MyPage/index.tsx
@@ -8,6 +8,9 @@ import EventCouponPage from './EventCouponPage'
 import TransactionHistoryPage from './TransactionHistoryPage'
 import PayHistoryPage from './PayHistoryPage'
 import ReportHistoryPage from './ReportHistoryPage'
+import InfoCard from './components/InfoCard/InfoCard'
+import Badge from '../../components/Badge/Badge'
+import Button from '../../components/Button/Button'
 
 const tabData = [
   { id: 'favorites', label: '관심 거래', content: <FavoritesPage /> },
@@ -40,10 +43,61 @@ const MyPage = () => {
   return (
     <>
       {/* 상단 */}
-      <section>
-        <div className="border-3 px-4 py-3">
-          <p className="text-fs20">top area</p>
-        </div>
+      <section className="mb-10 flex h-[280px] justify-between gap-5">
+        <InfoCard title="내 정보" showEditBtn={true}>
+          <div>
+            <h3 className="text-fs18 font-medium">닉네임네임 닉네임</h3>
+            <div className="text-fs14 mt-7 flex flex-col gap-3 text-gray-700">
+              <p>email@email.com</p>
+              <div className="flex items-center gap-[0.375rem]">
+                <Badge
+                  size="small"
+                  className="bg-lguplus w-fit py-1 leading-[100%]"
+                  label="LG U+"
+                />
+                <p>010-0000-0000</p>
+              </div>
+            </div>
+          </div>
+        </InfoCard>
+        <InfoCard title="내 데이터 정보">
+          <>
+            <div className="flex flex-col gap-3">
+              <div className="text-fs18 flex w-full justify-between font-medium text-gray-900">
+                <p>보유 데이터</p>
+                <p>1.9GB</p>
+              </div>
+              <div className="text-fs18 flex w-full justify-between font-medium text-gray-900">
+                <p>구매 데이터</p>
+                <p>600MB</p>
+              </div>
+              <div className="text-fs18 flex w-full justify-between font-medium text-gray-900">
+                <p>판매 가능 데이터</p>
+                <p>300MB</p>
+              </div>
+            </div>
+            <Button
+              text="데이터 전환하기"
+              className="text-fs18 border-pri-600 text-pri-600 border-[1.7px] font-medium"
+            />
+          </>
+        </InfoCard>
+        <InfoCard title="내 다챠페이 정보">
+          <div className="text-fs18 flex w-full justify-between font-medium text-gray-900">
+            <p>보유 다챠페이</p>
+            <p>10,400원</p>
+          </div>
+          <div className="flex gap-4">
+            <Button
+              text="충전하기"
+              className="text-fs18 border-pri-600 text-pri-600 w-full border-[1.7px] font-medium"
+            />
+            <Button
+              text="환전하기"
+              className="text-fs18 border-pri-600 text-pri-600 w-full border-[1.7px] font-medium"
+            />
+          </div>
+        </InfoCard>
       </section>
 
       {/* 탭 */}
