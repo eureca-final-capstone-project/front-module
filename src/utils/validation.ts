cb4c4f893097dc9b7ed29bb91895cae99d778bad
nue-os <kimsoeun0108@gmail.com>
2025-07-22 16:15:20 +0900

    [VT2-225] feat: 회원가입 입력 필드 유효성 검사 함수 구현

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -5,4 +5,13 @@ export const loginSchema = z.object({
   password: z.string().nonempty('비밀번호를 입력해 주세요.'),
 })
 
-export type LoginSchemaType = z.infer<typeof loginSchema> // 스키마 기반 TypeScript 타입 추론
+export const signUpSchema = z
+  .object({
+    email: z.email('올바른 이메일 형식을 입력해 주세요.'),
+    password: z.string().nonempty('비밀번호를 입력해 주세요.'),
+    passwordConfirm: z.string(),
+  })
+  .refine(data => data.passwordConfirm === data.password || data.passwordConfirm === '', {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['passwordConfirm'],
+  })
