cf9b6c092a8afbb80dd12ae79d800d4329d8dfdd
yeji424 <yejiborn424@gmail.com>
2025-07-23 01:47:50 +0900

    [VT2-226] feat: 체크박스 선택 및 삭제 버튼 연동 구현, 리스트에 반영

--- src/pages/MyPage/FavoritesPage.tsx
+++ src/pages/MyPage/FavoritesPage.tsx
@@ -1,42 +1,130 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useDeviceType } from '../../hooks/useDeviceType'
 import CheckBox from '../../components/CheckBox/CheckBox'
 import BasicModal from './components/Modal/BasicModal'
-import { modalTexts, buttonOptions } from './components/config'
+import { buttonOptions } from './components/config'
 import FavoritesHeader from './components/ButtonHeader/ButtonHeader'
+import PostCard, { PostCardProps } from '../../components/PostCard/PostCard'
+import React from 'react'
+import { useToast } from '../../hooks/useToast'
+import useSelect from '../../hooks/useSelect'
+import useModal from '../../hooks/useModal'
 
-type ModalType = keyof typeof modalTexts
+const dummyFavorites: PostCardProps[] = [
+  {
+    transactionFeedId: 1,
+    type: 'row',
+    telecomCompany: 'SKT',
+    // imageWrapperClassName: 'w-30 h-30',
+    defaultImageNumber: 1,
+    salesDataAmount: '1500',
+    title: 'SKT 데이터 팝니다',
+    nickname: '데이터왕',
+    createdAt: '2025.07.22 13:30',
+    liked: true,
+    onToggleLike: () => console.log('toggle like 1'),
+    salesType: 'deal',
+    salesPrice: 10000,
+    currentHeightPrice: 0,
+    status: 'active',
+    onClick: () => console.log('카드 클릭 1'),
+    favorite: true,
+  },
+  {
+    transactionFeedId: 2,
+    type: 'row',
+    telecomCompany: 'LG U+',
+    defaultImageNumber: 1,
+    salesDataAmount: '1500',
+    // imageWrapperClassName: 'w-30 h-30',
+    title: 'U+ 데이터 팝니다',
+    nickname: '데이터왕',
+    createdAt: '2025.07.22 13:30',
+    liked: true,
+    onToggleLike: () => console.log('toggle like 1'),
+    salesType: 'bid',
+    salesPrice: 10000,
+    currentHeightPrice: 0,
+    status: 'active',
+    onClick: () => console.log('카드 클릭 1'),
+    favorite: true,
+  },
+  {
+    transactionFeedId: 3,
+    type: 'row',
+    telecomCompany: 'KT',
+    defaultImageNumber: 1,
+    salesDataAmount: '800',
+    title: 'KT 데이터 저렴하게!',
+    nickname: '친절한KT',
+    createdAt: '어제',
+    liked: false,
+    onToggleLike: () => console.log('toggle like 2'),
+    salesType: 'bid',
+    salesPrice: 7000,
+    currentHeightPrice: 9500,
+    status: 'completed',
+    onClick: () => console.log('카드 클릭 2'),
+    favorite: true,
+    payhistory: true,
+    payhistorytime: '2025.07.22 13:30',
+    payhistorypay: 9500,
+  },
+]
+
+type ModalType = 'delete'
 
 const FavoritesPage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false)
-  const [modalType, setModalType] = useState<ModalType | null>(null)
+  const deviceType = useDeviceType()
+  const { showToast } = useToast()
+  const postIds = dummyFavorites.map(post => post.transactionFeedId)
+
+  const { selectedIds, toggleId, selectAll, clearAll, isSelected } = useSelect(postIds)
   const [selectedType, setSelectedType] = useState<'both' | 'deal' | 'bid'>('both')
+  const { modalType, isOpen: isModalOpen, openModal, closeModal } = useModal()
+
+  useEffect(() => {
+    console.log('--------선택 삭제:', selectedIds)
+  }, [selectedIds])
+
+  const gridColsClass = deviceType === 'mobile' ? 'grid-cols-1 bg-gray-10 p-4 ' : 'grid-cols-2'
+  const allChecked = postIds.length > 0 && postIds.every(id => selectedIds.includes(id))
 
   const handleSelectType = (value: string) => {
     if (value === 'both' || value === 'deal' || value === 'bid') {
       setSelectedType(value)
     }
   }
-  const deviceType = useDeviceType()
-  const gridColsClass = deviceType === 'mobile' ? 'grid-cols-1 bg-gray-10 p-4 ' : 'grid-cols-2'
 
-  const [checked, setChecked] = useState(false)
-
-  const openModal = (type: ModalType) => {
-    setModalType(type)
-    setIsModalOpen(true)
+  const toggleSelected = (id: number) => {
+    toggleId(id)
   }
 
-  const closeModal = () => {
-    setIsModalOpen(false)
-    setModalType(null)
+  const handleSelectAll = () => {
+    const isAllSelected = postIds.every(id => selectedIds.includes(id))
+    if (isAllSelected) {
+      clearAll()
+      console.log('---------전체 선택 해제: []')
+    } else {
+      selectAll()
+      console.log('---------전체 선택:', postIds)
+    }
+  }
+
+  const handleOpenModal = (type: ModalType) => {
+    if (selectedIds.length === 0) {
+      showToast({ type: 'error', msg: '삭제할 항목을 선택해주세요.' })
+      return
+    }
+    openModal(type)
   }
 
   const handleConfirmDelete = () => {
-    // 삭제 처리 로직
-    console.log('삭제 완료')
+    console.log('---------삭제 완료', selectedIds)
+    // 삭제 API 로직
     closeModal()
   }
+
   return (
     <div className="flex flex-col gap-5">
       {/* 상단 */}
@@ -44,28 +132,31 @@ const FavoritesPage = () => {
         buttonOptions={buttonOptions.fav}
         selectedType={selectedType}
         onSelectType={handleSelectType}
-        onOpenDeleteModal={openModal}
+        onOpenDeleteModal={handleOpenModal}
+        onSelectAll={handleSelectAll}
+        allChecked={allChecked}
       />
       {/* 콘텐츠 */}
       <div className={`grid gap-4 ${gridColsClass}`}>
-        <div className="flex gap-2">
-          <CheckBox
-            checked={checked}
-            onChange={() => setChecked(prev => !prev)}
-            type={deviceType === 'mobile' ? 'smallCheckBox' : 'default'}
-          />
-          <div>
-            <p>postCard 자리</p>
-          </div>
-        </div>
-        <div className="flex gap-2">
-          <CheckBox
-            checked={checked}
-            onChange={() => setChecked(prev => !prev)}
-            type={deviceType === 'mobile' ? 'smallCheckBox' : 'default'}
-          />
-          <div></div>
-        </div>
+        {dummyFavorites.map((post, index) => (
+          <React.Fragment key={index}>
+            <div className="flex items-start gap-2">
+              <CheckBox
+                checked={isSelected(post.transactionFeedId)}
+                onChange={() => toggleSelected(post.transactionFeedId)}
+                type={deviceType === 'mobile' ? 'smallCheckBox' : 'default'}
+              />
+              <div className="sm:bg-gray-10 sm:shadow-card-shadow w-full sm:block sm:rounded-[10px] sm:p-5">
+                <PostCard {...post} />
+              </div>
+            </div>
+
+            {/* 카드 div 바깥에서 hr 삽입 */}
+            {index < dummyFavorites.length - 1 && (
+              <hr className="border-0.5 block border-t border-gray-200 sm:hidden" />
+            )}
+          </React.Fragment>
+        ))}
       </div>
       {modalType && (
         <BasicModal
