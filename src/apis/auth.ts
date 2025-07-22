00d6adedb07f773835d817895f645a7e7f6f2a05
nue-os <kimsoeun0108@gmail.com>
2025-07-22 14:46:01 +0900

    [VT2-163] feat: 소셜 로그인용 리다이렉트 함수 kakaoLogin, googleLogin, naverLogin 추가

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -5,3 +5,15 @@ export const login = async (data: LoginSchemaType) => {
   const response = await client.post('/auth/login', data)
   return response.data
 }
+
+export const kakaoLogin = async () => {
+  window.location.href = `${import.meta.env.VITE_CLIENT_BASE_URL}/oauth2/authorization/kakao`
+}
+
+export const googleLogin = async () => {
+  window.location.href = `${import.meta.env.VITE_CLIENT_BASE_URL}/oauth2/authorization/google`
+}
+
+export const naverLogin = async () => {
+  window.location.href = `${import.meta.env.VITE_CLIENT_BASE_URL}/oauth2/authorization/kakao`
+}
