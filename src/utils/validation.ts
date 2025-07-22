866fafe91113d31e31ed370c0f6b75585bae2dbc
nue-os <kimsoeun0108@gmail.com>
2025-07-22 21:17:25 +0900

    [VT2-225] feat: 통신사 및 휴대폰 번호 유효성 검증 로직 추가

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -10,6 +10,11 @@ export const signUpSchema = z
     email: z.email('올바른 이메일 형식을 입력해 주세요.'),
     password: z.string().nonempty('비밀번호를 입력해 주세요.'),
     passwordConfirm: z.string(),
+    carrier: z.string().nonempty('통신사를 선택해 주세요.'),
+    phoneNumber: z
+      .string()
+      .nonempty('휴대폰 번호를 입력해 주세요.')
+      .regex(/^01[016789]-?\d{3,4}-?\d{4}$/, '올바른 휴대폰 번호 형식을 입력해 주세요.'),
   })
   .refine(data => data.passwordConfirm === data.password || data.passwordConfirm === '', {
     message: '비밀번호가 일치하지 않습니다.',
