14587260db80d4470c939a9c7e5c6cd8f0bf0db2
nue-os <kimsoeun0108@gmail.com>
2025-07-22 09:58:08 +0900

    [VT2-155] feat: 일반 로그인 페이지 반응형 구현

--- src/pages/LoginPage/components/LoginForm.tsx
+++ src/pages/LoginPage/components/LoginForm.tsx
@@ -45,7 +45,7 @@ const LoginForm = () => {
   }
 
   return (
-    <form onSubmit={handleSubmit(handleLogin)} className="flex flex-col gap-10">
+    <form onSubmit={handleSubmit(handleLogin)} className="flex flex-col gap-5 sm:gap-10">
       <Controller
         name="email"
         control={control}
@@ -87,7 +87,9 @@ const LoginForm = () => {
         text="로그인"
         type="submit"
         disabled={!isActive}
-        className="bg-pri-500 text-gray-10 disabled:bg-gray-50 disabled:text-gray-500"
+        className={
+          'border-gray-10 text-gray-10 sm:bg-pri-500 sm:text-gray-10 mt-15 border bg-transparent disabled:border-transparent disabled:bg-gray-50/50 disabled:text-gray-200 sm:mt-0 sm:border-none sm:disabled:bg-gray-50 sm:disabled:text-gray-500'
+        }
       />
     </form>
   )
--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -4,17 +4,19 @@ import LoginForm from './components/LoginForm'
 const LoginPage = () => {
   return (
     <main className="flex flex-col gap-10">
-      <h1 className="text-fs24 font-bold">로그인</h1>
-      <div className="space-y-7">
+      <h1 className="text-gray-10 sm:text-pri-900 text-fs24 text-center font-bold">로그인</h1>
+      <div className="space-y-4 sm:space-y-7">
         <LoginForm />
-        <div className="text-fs14 font-regular space-x-3 text-right text-gray-700">
+        <div className="text-fs14 font-regular space-x-3 text-right text-gray-100 sm:text-gray-700">
           <Button text="회원가입" onClick={() => {}} shape="underline" />
           <span>|</span>
           <Button text="비밀번호 찾기" onClick={() => {}} shape="underline" />
         </div>
       </div>
       <div className="space-y-8">
-        <div className="font-fs14 font-medium">SNS 계정으로 간편하게 시작하기</div>
+        <div className="text-gray-30 font-fs14 text-center font-medium sm:text-gray-900">
+          SNS 계정으로 간편하게 시작하기
+        </div>
         <div className="flex justify-evenly">
           <div>카카오 로그인</div>
           <div>구글 로그인</div>
