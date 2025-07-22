ed67780d8c92c1d778a9c6814cb6d66ce734da53
nue-os <kimsoeun0108@gmail.com>
2025-07-22 22:55:40 +0900

    [VT2-225] feat: 회원가입 요청 타입 관리

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -4,3 +4,13 @@ import { loginSchema, signUpSchema } from '../utils/validation'
 // 스키마 기반 TypeScript 타입 추론
 export type LoginSchemaType = z.infer<typeof loginSchema>
 export type SignUpSchemaType = z.infer<typeof signUpSchema>
+
+// 회원가입 request 타입
+export type SignUpRequestType = Omit<
+  z.infer<typeof signUpSchema>,
+  'passwordConfirm' | 'carrier'
+> & {
+  telecomCompanyId: number
+  phoneNumber: string
+  provider: string
+}
