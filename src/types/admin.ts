019e58672d8ab17d04393f878cf24e58f81097d8
nue-os <kimsoeun0108@gmail.com>
2025-07-25 01:27:02 +0900

    [VT2-300] feat: 관리자 회원 내역 타입 정의

---
+++ src/types/admin.ts
@@ -0,0 +1,11 @@
+export interface UserHistory {
+  userId: string
+  email: string
+  nickname: string
+  telecomCompany: string
+  phoneNumber: string
+  createdAt: string
+  reportCount: number
+  status: string
+  isBlocked: boolean
+}
