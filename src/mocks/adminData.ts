4b344d4078aca3fde3aef28b0a6c3201e031d0cc
nue-os <kimsoeun0108@gmail.com>
2025-07-25 01:28:02 +0900

    [VT2-300] feat: 관리자 회원 내역 mock 데이터 생성

---
+++ src/mocks/adminData.ts
@@ -0,0 +1,24 @@
+export const userHistoryData = [
+  {
+    userId: '1',
+    email: 'abc@datch.com',
+    nickname: '몽실몽실구름빵',
+    telecomCompany: 'SKT',
+    phoneNumber: '010-1234-5678',
+    createdAt: '2025.07.10',
+    reportCount: 2,
+    status: '활성',
+    isBlocked: false,
+  },
+  {
+    userId: '2',
+    email: 'def@datch.com',
+    nickname: '빛나는별',
+    telecomCompany: 'KT',
+    phoneNumber: '010-2345-6789',
+    createdAt: '2023.03.21',
+    reportCount: 0,
+    status: '비활성',
+    isBlocked: true,
+  },
+]
