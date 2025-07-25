a7e268bacc515568e478dd0fef409dde19d817cb
nue-os <kimsoeun0108@gmail.com>
2025-07-25 12:53:44 +0900

    [VT2-300] style: 회원 신고 내역 테이블 구조 변경

--- src/pages/AdminPage/components/UserDetailRow.tsx
+++ src/pages/AdminPage/components/UserDetailRow.tsx
@@ -8,31 +8,39 @@ interface UserDetailRowProps {
 
 const UserDetailRow = ({ reports }: UserDetailRowProps) => {
   return (
-    <>
-      {reports &&
-        reports.length > 0 &&
-        reports.map(report => {
-          return (
-            <tr key={report.reportId} className="border-t border-gray-100 bg-gray-50">
-              <td></td>
-              <td className="px-3 py-4">{report.reportId}</td>
-              <td className="px-3 py-4">{report.reportType}</td>
-              <td
-                className="max-w-85 overflow-hidden px-3 py-4 text-ellipsis whitespace-nowrap"
-                colSpan={4}
-              >
-                {report.content}
-              </td>
-              <td className="px-3 py-4" colSpan={2}>
-                {new Date(report.createdAt).toLocaleDateString()}
-              </td>
-              <td className="px-3 py-4" colSpan={2}>
-                <Badge label={report.status} {...STATUS_STYLE[report.status]} />
-              </td>
-            </tr>
-          )
-        })}
-    </>
+    <table className="w-full">
+      <colgroup>
+        <col style={{ width: '4%' }} />
+        <col style={{ width: '3%' }} />
+        <col style={{ width: '20%' }} />
+        <col style={{ width: '31.5%' }} />
+      </colgroup>
+      <tbody>
+        {reports &&
+          reports.length > 0 &&
+          reports.map(report => {
+            return (
+              <tr key={report.reportId} className="border-t border-gray-100 bg-gray-50">
+                <td></td>
+                <td className="px-3 py-4">{report.reportId}</td>
+                <td className="px-3 py-4">{report.reportType}</td>
+                <td
+                  className="max-w-50 overflow-hidden px-3 py-4 text-ellipsis whitespace-nowrap"
+                  colSpan={4}
+                >
+                  {report.content}
+                </td>
+                <td className="px-3 py-4" colSpan={2}>
+                  {new Date(report.createdAt).toLocaleDateString()}
+                </td>
+                <td className="px-3 py-4" colSpan={2}>
+                  <Badge label={report.status} {...STATUS_STYLE[report.status]} />
+                </td>
+              </tr>
+            )
+          })}
+      </tbody>
+    </table>
   )
 }
 
