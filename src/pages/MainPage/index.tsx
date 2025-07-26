276ea896c1700154ae8d8a46fe816d42cfe5ccfd
H-JuKyung <hjk000hjk17@gmail.com>
2025-07-26 13:06:18 +0900

    [VT2-337] chore: 상단 멘트 및 하단 CTA 섹션 삭제

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -9,7 +9,6 @@ import Graph from '../../components/Graph/Graph'
 import { lineData } from '../../mocks/graphData'
 import LatestSection from './components/LatestSection'
 import SectionDescription from './components/SectionDescription'
-import CallToActionSection from './components/CallToActionSection'
 import FadeInUpMotion from '../../components/Animation/FadeInUpMotion'
 
 const MainPage = () => {
@@ -21,24 +20,9 @@ const MainPage = () => {
         {/* 이벤트 배너 */}
         <EventBanner />
         <Container className="my-6 max-w-[1312px] overflow-x-hidden overflow-y-hidden sm:my-10 sm:px-4">
-          {/* 상단 멘트 */}
-          <FadeInUpMotion custom={1}>
-            <Card className="border-pri-500 mb-6 flex min-h-50 justify-center rounded-none border-8 p-5 text-center sm:mb-10 sm:rounded-md sm:py-12 lg:h-60">
-              <h1 className="leading-tight font-extrabold text-gray-900">
-                <span className="mb-2 block text-2xl sm:text-3xl lg:text-4xl">당신의 데이터,</span>
-                <span className="text-pri-600 block text-3xl drop-shadow-md sm:text-4xl lg:text-5xl">
-                  새로운 가치를 만나다.
-                </span>
-              </h1>
-              <p className="text-fs14 text-gray-800 sm:text-lg">
-                투명하고 안전한 플랫폼에서 <br className="block sm:hidden" />
-                <strong>데이터의 잠재력</strong>을 최대한 활용하세요.
-              </p>
-            </Card>
-          </FadeInUpMotion>
           <div className="grid grid-cols-1 gap-6 sm:gap-y-10 lg:grid-cols-2 lg:gap-x-7">
             {/* 시세 그래프 섹션 */}
-            <FadeInUpMotion custom={2}>
+            <FadeInUpMotion custom={1}>
               <div className="flex flex-col">
                 <SectionDescription text="통신사 별 <span class='text-pri-500 font-bold'>데이터 거래 시세 변동</span>을 한눈에 확인하세요." />
                 <Card className="flex min-h-60 justify-start overflow-hidden rounded-none p-5 sm:h-110 sm:rounded-b-md">
@@ -48,7 +32,7 @@ const MainPage = () => {
               </div>
             </FadeInUpMotion>
             {/* 추천 상품 섹션 */}
-            <FadeInUpMotion custom={3}>
+            <FadeInUpMotion custom={2}>
               <div className="flex flex-col">
                 <SectionDescription text="당신을 위한 <span class='text-pri-500 font-bold'>맞춤형 데이터 상품</span>을 확인하세요." />
                 <Card className="flex min-h-75 justify-start overflow-hidden rounded-none p-5 sm:h-110 sm:rounded-b-md">
@@ -58,7 +42,7 @@ const MainPage = () => {
               </div>
             </FadeInUpMotion>
             {/* 진행중인 경매 섹션 */}
-            <FadeInUpMotion custom={4}>
+            <FadeInUpMotion custom={3}>
               <div className="flex flex-col">
                 <SectionDescription text="현재 <span class='text-pri-500 font-bold'>활발히 거래되고 있는 경매</span> 상품을 확인하세요." />
                 <Card className="flex min-h-75 justify-start overflow-hidden rounded-none p-5 sm:h-110 sm:rounded-b-md">
@@ -76,7 +60,7 @@ const MainPage = () => {
               </div>
             </FadeInUpMotion>
             {/* 최신 상품 섹션 */}
-            <FadeInUpMotion custom={5}>
+            <FadeInUpMotion custom={4}>
               <div className="flex flex-col">
                 <SectionDescription text="방금 등록된 <span class='text-pri-500 font-bold'>따끈따끈한 최신 데이터 상품</span>입니다." />
                 <Card className="flex min-h-75 justify-start overflow-hidden rounded-none p-5 sm:h-110 sm:rounded-b-md">
@@ -91,10 +75,6 @@ const MainPage = () => {
               </div>
             </FadeInUpMotion>
           </div>
-          {/* CTA 섹션 */}
-          <FadeInUpMotion custom={6}>
-            <CallToActionSection />
-          </FadeInUpMotion>
         </Container>
       </main>
     </div>
