a7e81f12030109d5d142528befa2b1ff4aa76768
nue-os <kimsoeun0108@gmail.com>
2025-07-26 19:35:51 +0900

    [VT2-354] feat: 회원 정보 조회 API 연동하여 통신사 정보 받아오도록 구현

--- src/pages/PostWritePage/components/CarrierField.tsx
+++
@@ -1,8 +0,0 @@
-import Badge from '../../../components/Badge/Badge'
-import { getTelecomBadgeColor } from '../../../utils/telecom'
-
-const CarrierField = () => {
-  return <Badge label={'LG U+'} className={getTelecomBadgeColor('LG U+')} size="small" />
-}
-
-export default CarrierField
