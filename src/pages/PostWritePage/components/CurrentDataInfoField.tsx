28ee1de0e283126a14a5a1594430b4d4210781f8
nue-os <kimsoeun0108@gmail.com>
2025-07-26 19:41:32 +0900

    [VT2-354] feat: 회원 데이터 정보 조회 API 연동

--- src/pages/PostWritePage/components/CurrentDataInfoField.tsx
+++ src/pages/PostWritePage/components/CurrentDataInfoField.tsx
@@ -1,15 +1,30 @@
+import { useQuery } from '@tanstack/react-query'
+import { getUserDataStatus } from '../../../apis/userInfo'
 import { formatDataSize } from '../../../utils/format'
 
 const CurrentDataInfoField = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['userDataStatus'],
+    queryFn: getUserDataStatus,
+  })
+
+  if (isLoading) {
+    return <div className="text-fs14 text-gray-400">데이터 정보를 불러오는 중...</div>
+  }
+
+  if (isError || !data) {
+    return <div className="text-fs14 text-error">데이터 정보를 불러올 수 없습니다.</div>
+  }
+
   return (
     <div className="space-y-3">
       <div className="flex justify-between">
         <div>보유 데이터</div>
-        <div>{formatDataSize(1000)}</div>
+        <div>{formatDataSize(data.totalDataMb)}</div>
       </div>
       <div className="flex justify-between">
         <div>판매 가능 데이터</div>
-        <div>{formatDataSize(100)}</div>
+        <div>{formatDataSize(data.sellableDataMb)}</div>
       </div>
     </div>
   )
