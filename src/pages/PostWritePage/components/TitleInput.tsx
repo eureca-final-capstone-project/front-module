7a8602d7d7090f90ea91e3afdf438a3fce3092a9
nue-os <kimsoeun0108@gmail.com>
2025-07-26 15:56:47 +0900

    [VT2-354] refactor: 공통 필드 구조 리팩토링

--- src/pages/PostWritePage/components/TitleInput.tsx
+++ src/pages/PostWritePage/components/TitleInput.tsx
@@ -1,6 +1,5 @@
 import { Controller, useFormContext } from 'react-hook-form'
 import Input from '../../../components/Input/Input'
-import Card from '../../../components/Card/Card'
 
 const TitleInput = () => {
   const {
@@ -10,27 +9,24 @@ const TitleInput = () => {
   } = useFormContext()
 
   return (
-    <Card>
-      <div className="text-fs20 border-b border-gray-100 pb-2 font-medium">제목</div>
-      <Controller
-        name="title"
-        control={control}
-        defaultValue=""
-        render={({ field }) => (
-          <Input
-            label="판매글 제목을 작성해 주세요."
-            id="title"
-            value={field.value}
-            onChange={e => {
-              field.onChange(e)
-              if (errors.title) clearErrors('title')
-            }}
-            error={!!errors.title}
-            errorMsg={errors.title?.message?.toString() || ''}
-          />
-        )}
-      />
-    </Card>
+    <Controller
+      name="title"
+      control={control}
+      defaultValue=""
+      render={({ field }) => (
+        <Input
+          label="판매글 제목을 작성해 주세요."
+          id="title"
+          value={field.value}
+          onChange={e => {
+            field.onChange(e)
+            if (errors.title) clearErrors('title')
+          }}
+          error={!!errors.title}
+          errorMsg={errors.title?.message?.toString() || ''}
+        />
+      )}
+    />
   )
 }
 
