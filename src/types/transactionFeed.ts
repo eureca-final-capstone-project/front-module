6244105c455c359ddca0f8f8256b5269f9365d4b
nue-os <kimsoeun0108@gmail.com>
2025-07-26 20:07:41 +0900

    [VT2-354] feat: 판매글 작성 타입에서 unit 제외 후 페이로드 타입 재정의

--- src/types/transactionFeed.ts
+++ src/types/transactionFeed.ts
@@ -2,3 +2,5 @@ import z from 'zod'
 import { postTransactionSchema } from '../utils/validation'
 
 export type PostTransactionType = z.infer<typeof postTransactionSchema>
+
+export type PostTransactionPayloadType = Omit<PostTransactionType, 'unit'>
