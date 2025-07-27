9e1ccc3ee5c12d1f8f0d7f7954c1d73d7104b1b6
yeji424 <yejiborn424@gmail.com>
2025-07-27 21:57:01 +0900

    [VT2-230] feat: ListTile 컴포넌트에 onClick 기능 추가

--- src/components/ListTile/ListTile.tsx
+++ src/components/ListTile/ListTile.tsx
@@ -1,15 +1,20 @@
 interface ListTileProps {
   children: React.ReactNode
   type?: 'title'
+  onClick?: () => void
 }
 
-const ListTile = ({ children, type }: ListTileProps) => {
+const ListTile = ({ children, type, onClick }: ListTileProps) => {
   const fontSizeClass = type === 'title' ? 'text-fs14 lg:text-fs16' : 'text-fs16 lg:text-fs18'
   const baseClass = `${fontSizeClass} flex w-full justify-between rounded-md`
   const typeClass =
-    type === 'title' ? 'bg-background px-5 py-0' : 'shadow-tile bg-gray-10 px-5 py-6'
+    type === 'title' ? 'bg-background px-5 py-0' : 'cursor-pointer shadow-tile bg-gray-10 px-5 py-6'
 
-  return <div className={`${baseClass} ${typeClass}`}>{children}</div>
+  return (
+    <div onClick={onClick} className={`${baseClass} ${typeClass}`}>
+      {children}
+    </div>
+  )
 }
 
 export default ListTile
