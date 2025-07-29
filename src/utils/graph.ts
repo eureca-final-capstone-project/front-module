a3d15954bdbbaa5d44760c2d6bca363293283445
nue-os <kimsoeun0108@gmail.com>
2025-07-29 13:20:51 +0900

    [VT2-411] fix: Graph에서 통신사 entry 속성 대문자로 변경

--- src/utils/graph.ts
+++ src/utils/graph.ts
@@ -4,9 +4,9 @@ export const transformToGraphData = (data: HourlyStatistic[]) => {
   return data.map(item => {
     const entry: Record<string, number | null> = { hour: item.hour }
     item.pricesByCarrier.forEach(carrier => {
-      if (carrier.carrierName === 'SKT') entry['skt'] = carrier.pricePerMb
-      if (carrier.carrierName === 'KT') entry['kt'] = carrier.pricePerMb
-      if (carrier.carrierName === 'LG U+') entry['u+'] = carrier.pricePerMb
+      if (carrier.carrierName === 'SKT') entry['SKT'] = carrier.pricePerMb
+      if (carrier.carrierName === 'KT') entry['KT'] = carrier.pricePerMb
+      if (carrier.carrierName === 'LG U+') entry['LG U+'] = carrier.pricePerMb
     })
     return entry
   })
