3ea5aa066d454ca5cacfbde9d7f65b22f6cca96f
H-JuKyung <hjk000hjk17@gmail.com>
2025-07-29 17:58:50 +0900

    [VT2-361] feat: 검색바 기능 및 UI/UX 개선
    - 입력값 지우기(X) 버튼 추가 및 동적 표시
    - `defaultValue` 변경 시 입력 필드 값 자동 업데이트
    - 검색 아이콘을 폼 제출 버튼으로 기능 변경
    - 포커스 시 테두리/아이콘 색상 변경

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,7 @@
 import { useForm } from 'react-hook-form'
 import SearchIcon from '@/assets/icons/search.svg?react'
+import DeleteIcon from '@/assets/icons/delete.svg?react'
+import { useEffect } from 'react'
 
 interface SearchBarProps {
   onSubmit: (keyword: string) => void
@@ -8,27 +10,48 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ onSubmit, defaultValue = '', className }: SearchBarProps) => {
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, watch, setValue } = useForm({
     defaultValues: { keyword: defaultValue },
   })
+
+  const keyword = watch('keyword')
 
   const submit = (data: { keyword: string }) => {
     onSubmit(data.keyword.trim())
   }
+
+  const clearInput = () => {
+    setValue('keyword', '')
+  }
+
+  useEffect(() => {
+    setValue('keyword', defaultValue)
+  }, [defaultValue, setValue])
 
   return (
     <form
       onSubmit={handleSubmit(submit)}
       className="relative z-10 flex flex-1 justify-center self-end sm:translate-y-[6px]"
     >
-      <div className={`relative w-full ${className}`}>
-        <SearchIcon className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-400" />
+      <div className={`focus-within:text-pri-500 relative w-full text-gray-400 ${className}`}>
+        <button type="submit" className="absolute top-1/2 left-3 -translate-y-1/2">
+          <SearchIcon />
+        </button>
         <input
           {...register('keyword')}
           type="text"
           placeholder="검색할 내용을 입력해 주세요"
-          className="placeholder:font-regular bg-gray-10 w-full rounded-[32px] border border-gray-400 p-3 pl-9 placeholder:text-gray-400 focus:outline-none"
+          className="placeholder:font-regular bg-gray-10 focus:border-pri-500 w-full rounded-full border border-gray-400 p-3 pl-9 text-gray-900 placeholder:text-gray-400 focus:outline-none"
         />
+        {keyword && (
+          <button
+            type="button"
+            onClick={clearInput}
+            className="absolute top-1/2 right-3 -translate-y-1/2"
+          >
+            <DeleteIcon />
+          </button>
+        )}
       </div>
     </form>
   )
