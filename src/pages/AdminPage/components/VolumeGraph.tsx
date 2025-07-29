d3b66ec73dfbc9f088cca830c01af1a93ab3ca95
nue-os <kimsoeun0108@gmail.com>
2025-07-29 18:39:21 +0900

    [VT2-428] feat: 관리자 대시보드 거래량 그래프 API 연동

--- src/pages/AdminPage/components/VolumeGraph.tsx
+++ src/pages/AdminPage/components/VolumeGraph.tsx
@@ -1,9 +1,27 @@
+import { useQuery } from '@tanstack/react-query'
 import Card from '../../../components/Card/Card'
 import Graph from '../../../components/Graph/Graph'
 import ToggleText from '../../../components/Toggle/ToggleText'
-import { barData } from '../../../mocks/graphData'
+import { getVolumeData } from '../../../apis/admin/dashboard'
+import { useState } from 'react'
 
 const VolumeGraph = () => {
+  const [salesType, setSalesType] = useState('일반 판매')
+
+  const {
+    data: statistics,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['volume', salesType],
+    queryFn: () => getVolumeData(salesType),
+  })
+
+  if (isLoading) return <div>데이터를 불러오는 중입니다...</div>
+  if (isError || !statistics?.data) return <div>데이터를 불러오지 못했습니다.</div>
+
+  const { volumes } = statistics.data
+
   return (
     <section className="flex flex-col gap-5">
       <div className="flex justify-between">
@@ -11,12 +29,13 @@ const VolumeGraph = () => {
         <ToggleText
           leftText="일반 판매"
           rightText="입찰 판매"
-          onToggle={state => console.log('텍스트 토글 상태:', state)}
+          onToggle={setSalesType}
+          selectedText={salesType}
         />
       </div>
 
       <Card className="px-0">
-        <Graph type="bar" data={barData} yKeys={['거래량']} height={328} />
+        <Graph type="bar" data={volumes} name="거래량" yKeys={['saleVolume']} height={328} />
       </Card>
     </section>
   )
