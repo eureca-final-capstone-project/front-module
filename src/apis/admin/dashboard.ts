76f8631360557256e89b636c1d3fd5c8cefb98b8
nue-os <kimsoeun0108@gmail.com>
2025-07-29 21:32:44 +0900

    [VT2-300] feat: 회원 내역 조회 함수 구현

--- src/apis/admin/dashboard.ts
+++ src/apis/admin/dashboard.ts
@@ -1,4 +1,5 @@
 import admin from '.'
+import { Pageable } from '../transactionFeed'
 
 export const getDashboardData = async () => {
   const response = await admin.get('/dashboard')
@@ -7,5 +8,12 @@ export const getDashboardData = async () => {
 
 export const getVolumeData = async (salesType?: string) => {
   const response = await admin.get('/dashboard/volume-stats', { params: { salesType } })
+  return response.data
+}
+
+export const getUsers = async (pageable: Pageable) => {
+  const response = await admin.get('/users', {
+    params: pageable,
+  })
   return response.data
 }
