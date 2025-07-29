03b842db3bd417107dcd50370cc11b6aadfc9931
nue-os <kimsoeun0108@gmail.com>
2025-07-29 19:02:54 +0900

    [VT2-428] fix: 토큰 갱신 실패 시이동하는 페이지 설정을 위해 isAdmin 파라미터 추가하여 회원/관리자 구분

--- src/apis/admin/index.ts
+++ src/apis/admin/index.ts
@@ -12,7 +12,7 @@ const admin = axios.create({
 // 요청 시 accessToken 자동 주입 인터셉터 등록
 attachRequestInterceptor(admin, 'adminAccessToken')
 
-// 응답 시 401 감지 및 자동 토큰 갱신 인터셉터 등록
-attachResponseInterceptor(admin, 'adminAccessToken')
+// 응답 시 토큰 만료 감지 및 자동 토큰 갱신 인터셉터 등록
+attachResponseInterceptor(admin, 'adminAccessToken', true)
 
 export default admin
