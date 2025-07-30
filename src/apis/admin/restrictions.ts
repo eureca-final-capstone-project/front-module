262d9989b8645315d38a02daa53ec1bc896a6fd9
nue-os <kimsoeun0108@gmail.com>
2025-07-30 20:53:28 +0900

    [VT2-303] feat: 제재 내역 및 제재별 신고 내역 조회 API 호출 함수 구현

--- src/apis/admin/restrictions.ts
+++ src/apis/admin/restrictions.ts
@@ -1,4 +1,5 @@
 import admin from '.'
+import { Pageable } from '../transactionFeed'
 
 export const restrictionAccept = async (restrictionTargetId: number) => {
   const response = await admin.patch(`/restrict/${restrictionTargetId}/accept`)
@@ -7,5 +8,25 @@ export const restrictionAccept = async (restrictionTargetId: number) => {
 
 export const restrictionReject = async (restrictionTargetId: number) => {
   const response = await admin.patch(`/restrict/${restrictionTargetId}/reject`)
+  return response.data
+}
+
+export const getRestrictions = async ({
+  keyword,
+  statusCode,
+  pageable,
+}: {
+  keyword?: string
+  statusCode?: string
+  pageable: Pageable
+}) => {
+  const response = await admin.get('/restrictions', {
+    params: { ...pageable, ...(keyword ? { keyword } : {}), ...(statusCode ? { statusCode } : {}) },
+  })
+  return response.data
+}
+
+export const getRestrictinoReport = async (restrictId: number) => {
+  const response = await admin.get(`/restricts/${restrictId}/report-list`)
   return response.data
 }
