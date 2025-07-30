43ea1e67bb51e926fce6a3436dbae510b0dfb591
nue-os <kimsoeun0108@gmail.com>
2025-07-30 12:02:34 +0900

    [VT2-301] feat: 관리자 신고 내역 API 연동

--- src/pages/AdminPage/ReportHistory.tsx
+++ src/pages/AdminPage/ReportHistory.tsx
@@ -1,15 +1,17 @@
-import { useSearchParams } from 'react-router-dom'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import SearchBar from '../../components/SearchBar/SearchBar'
 import { useState } from 'react'
 import Pagination from '../../components/Pagination/Pagination'
 import { useQuery } from '@tanstack/react-query'
-import { reportsData } from '../../mocks/adminData'
 import Table from '../../components/Table/Table'
 import { REPORT_STATUS_LABEL, reportColumns, STATUS_STYLE } from '../../constants/admin'
 import Badge from '../../components/Badge/Badge'
 import { Report } from '../../types/admin'
+import { getReports } from '../../apis/admin/reports'
 
 const ReportHistroy = () => {
+  const navigate = useNavigate()
+
   const [searchParams, setSearchParams] = useSearchParams()
   const pageParam = parseInt(searchParams.get('page') ?? '1', 10)
 
@@ -24,10 +26,18 @@ const ReportHistroy = () => {
     size: itemsPerPage,
   }
 
-  //   const { data, isLoading, isError } = useQuery({
-  //     queryKey: ['users', currentPage, keyword],
-  //     queryFn: () => getUsers({ keyword, pageable }),
-  //   })
+  const statusCode = '' // 임시
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['reports', currentPage, keyword],
+    queryFn: () => getReports({ keyword, statusCode, pageable }),
+  })
+
+  if (isLoading) return <div>데이터를 불러오는 중입니다...</div>
+  if (isError || !data?.data) return <div>데이터를 불러오지 못했습니다.</div>
+
+  const { totalElements, totalPages, content: reportsData } = data.data
+  console.log(reportsData)
 
   const renderReportCell = (key: keyof Report, row: Report) => {
     const { reportedAt, status } = row
@@ -36,6 +46,7 @@ const ReportHistroy = () => {
       case 'reportedAt':
         return new Date(reportedAt).toLocaleDateString()
       case 'status': {
+        console.log(status)
         return (
           <Badge
             label={REPORT_STATUS_LABEL[status]}
@@ -63,22 +74,27 @@ const ReportHistroy = () => {
     <main className="space-y-13">
       <SearchBar defaultValue={keyword} onSubmit={handleSearch} />
       <section className="space-y-7">
-        <h1 className="text-fs24 font-medium">회원 내역</h1>
+        <h1 className="text-fs24 font-medium">신고 내역</h1>
         <div className="relative">
           <div className="absolute -top-5 right-0 -translate-y-full font-medium">
-            Total <span className="text-pri-400">{4}</span>, Page
-            <span className="text-pri-400"> {currentPage}</span>/{1}
+            Total <span className="text-pri-400">{totalElements}</span>, Page
+            <span className="text-pri-400"> {currentPage}</span>/{totalPages}
           </div>
           <Table
             columns={reportColumns}
             data={reportsData}
             renderCell={renderReportCell}
-            // isClickable={row => row.reportCount > 0 || row.email.length >= 18}
-            // onRowClick={handleRowClick}
+            isArrow={false}
+            isClickable={() => true}
+            onRowClick={row => navigate(`/report/${row.reportHistoryId}`)}
           />
         </div>
       </section>
-      <Pagination currentPage={currentPage} totalPages={4} onPageChange={handlePageChange} />
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        onPageChange={handlePageChange}
+      />
     </main>
   )
 }
