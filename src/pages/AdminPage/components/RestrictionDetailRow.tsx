dc5bde4e9d51ce51795569f409e88a828603e3c8
nue-os <kimsoeun0108@gmail.com>
2025-07-30 20:51:34 +0900

    [VT2-303] feat: 제재별 신고 내역 테이블 구현

---
+++ src/pages/AdminPage/components/RestrictionDetailRow.tsx
@@ -0,0 +1,49 @@
+import Badge from '../../../components/Badge/Badge'
+import { REPORT_STATUS_LABEL, STATUS_STYLE } from '../../../constants/admin'
+import { RestrictionReport } from '../../../types/admin'
+
+interface RestrictionDetailRowwProps {
+  reports: RestrictionReport[]
+}
+
+const RestrictionDetailRow = ({ reports }: RestrictionDetailRowwProps) => {
+  return (
+    <table className="w-full">
+      <colgroup>
+        <col style={{ width: '4%' }} />
+        <col style={{ width: '5%' }} />
+        <col style={{ width: '10%' }} />
+        <col style={{ width: '28%' }} />
+        <col style={{ width: '10%' }} />
+        <col style={{ width: '15%' }} />
+      </colgroup>
+
+      <tbody>
+        {reports &&
+          reports.length > 0 &&
+          reports.map(report => {
+            console.log(report)
+            return (
+              <tr key={report.reportId} className="bg-gray-30 border-t border-gray-100">
+                <td></td>
+                <td className="px-3 py-4">{report.reportId}</td>
+                <td className="px-3 py-4">{report.reportType}</td>
+                <td className="max-w-50 overflow-hidden px-3 py-4 text-ellipsis whitespace-nowrap">
+                  {report.content}
+                </td>
+                <td className="px-3 py-4">{new Date(report.reportedAt).toLocaleDateString()}</td>
+                <td className="px-3 py-4">
+                  <Badge
+                    label={REPORT_STATUS_LABEL[report.status]}
+                    {...STATUS_STYLE[report.status]}
+                  />
+                </td>
+              </tr>
+            )
+          })}
+      </tbody>
+    </table>
+  )
+}
+
+export default RestrictionDetailRow
