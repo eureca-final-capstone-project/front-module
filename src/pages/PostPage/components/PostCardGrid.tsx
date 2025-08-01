28bee868979f962addf1c1fff803fcd3f0ae1f7c
H-JuKyung <hjk000hjk17@gmail.com>
2025-08-01 16:32:13 +0900

    [VT2-98] feat: 스켈레톤 적용

--- src/pages/PostPage/components/PostCardGrid.tsx
+++ src/pages/PostPage/components/PostCardGrid.tsx
@@ -1,13 +1,44 @@
 import PostCard from '../../../components/PostCard/PostCard'
 import type { PostCardProps } from '../../../components/PostCard/PostCard'
+import PostCardColSkeleton from '../../../components/PostCard/PostCardColSkeleton'
+import PostCardRowSkeleton from '../../../components/PostCard/PostCardRowSkeleton'
 import { useDeviceType } from '../../../hooks/useDeviceType'
 
 interface PostCardGridProps {
   posts: PostCardProps[]
+  isLoading: boolean
 }
 
-const PostCardGrid = ({ posts }: PostCardGridProps) => {
+const PostCardGrid = ({ posts, isLoading }: PostCardGridProps) => {
   const deviceType = useDeviceType()
+
+  if (isLoading) {
+    const skeletonCount = 12
+    return (
+      <div
+        className={`min-w-0 ${
+          deviceType === 'mobile'
+            ? 'bg-gray-10 grid min-w-0 grid-cols-1 gap-4 p-4'
+            : deviceType === 'tablet'
+              ? 'grid grid-cols-1 gap-x-6 gap-y-15.5 pb-10 md:grid-cols-2'
+              : 'grid grid-cols-3 gap-x-6 gap-y-15.5 pb-10 xl:grid-cols-4'
+        }`}
+      >
+        {Array.from({ length: skeletonCount }).map((_, idx) =>
+          deviceType === 'mobile' ? (
+            <div
+              key={idx}
+              className={`pb-4 ${idx !== skeletonCount - 1 ? 'border-b-[0.5px] border-gray-200' : ''}`}
+            >
+              <PostCardRowSkeleton />
+            </div>
+          ) : (
+            <PostCardColSkeleton key={idx} />
+          )
+        )}
+      </div>
+    )
+  }
 
   return (
     <div
