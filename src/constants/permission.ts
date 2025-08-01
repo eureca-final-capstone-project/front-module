fb2c75b0f887629c7ba07cbec2b0ca39d3e83b14
nue-os <kimsoeun0108@gmail.com>
2025-08-01 16:49:38 +0900

    [VT2-476] feat: 제재별 안내 문구 상수화

---
+++ src/constants/permission.ts
@@ -0,0 +1,8 @@
+export const PERMISSIONS = ['WRITE', 'TRANSACTION', 'DATA_TRANSFER', 'PAY_CHARGE']
+
+export const PERMISSION_MESSAGES = {
+  WRITE: '판매글 작성/수정 불가',
+  DATA_TRANSFER: '데이터 전환 불가',
+  TRANSACTION: '거래 불가',
+  PAY_CHARGE: '페이 충전 불가',
+}
