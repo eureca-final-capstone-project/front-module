fc90594f0f60cbf1e84a01091d09478caf52d5d2
nue-os <kimsoeun0108@gmail.com>
2025-08-04 00:22:44 +0900

    [VT2-484] feat: 이용 약관 모달 구현 및 내용 작성

--- src/pages/SignUpPage/components/Agreement.tsx
+++ src/pages/SignUpPage/components/Agreement.tsx
@@ -1,7 +1,10 @@
-import { agreements } from '../../../constants/agreements'
+import { agreementDetails, agreements } from '../../../constants/agreements'
 import CheckBox from '../../../components/CheckBox/CheckBox'
 import Button from '../../../components/Button/Button'
 import { useDeviceType } from '../../../hooks/useDeviceType'
+import { useState } from 'react'
+import Modal from '../../../components/Modal/Modal'
+import CloseIcon from '@/assets/icons/x.svg?react'
 
 interface AgreementProps {
   checked: Record<string, boolean>
@@ -10,6 +13,8 @@ interface AgreementProps {
 
 const Agreement = ({ checked, onChange }: AgreementProps) => {
   const deviceType = useDeviceType()
+
+  const [openedAgreementId, setOpenedAgreementId] = useState<string | null>(null)
 
   const allChecked = agreements.every(({ id }) => checked[id])
 
@@ -45,11 +50,34 @@ const Agreement = ({ checked, onChange }: AgreementProps) => {
               text="내용 보기"
               shape="underline"
               className="text-fs12 text-gray-200 sm:text-gray-400"
-              onClick={() => {}}
+              onClick={() => setOpenedAgreementId(id)}
             />
           </div>
         ))}
       </div>
+
+      {openedAgreementId && (
+        <Modal
+          isOpen={!!openedAgreementId}
+          onClose={() => setOpenedAgreementId(null)}
+          className="relative max-w-139"
+        >
+          <h2 className="text-fs20 text-center font-semibold">
+            {agreements.find(a => a.id === openedAgreementId)?.label || '약관 상세'}
+          </h2>
+          <CloseIcon
+            className="absolute top-6 right-6 h-3.5 w-3.5 cursor-pointer text-gray-400 hover:text-gray-900"
+            onClick={() => setOpenedAgreementId(null)}
+          />
+
+          {agreementDetails[openedAgreementId].map(({ title, content }, index) => (
+            <section key={index} className="mt-8 space-y-3">
+              <h3 className="font-semibold">{title}</h3>
+              <p className="text-fs14 whitespace-pre-wrap">{content}</p>
+            </section>
+          ))}
+        </Modal>
+      )}
     </div>
   )
 }
