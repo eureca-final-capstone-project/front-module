29fb35fc5d6aaf9d41d9faa7869de6af9e8553b7
yeji424 <yejiborn424@gmail.com>
2025-08-03 20:56:23 +0900

    [VT2-460] fix: 모바일 메뉴바, 알림창 열릴 때 페이지 스크롤 유지되도록 개선

--- src/hooks/useScrollBlock.ts
+++ src/hooks/useScrollBlock.ts
@@ -4,25 +4,25 @@ export const useScrollBlock = (block: boolean) => {
   useEffect(() => {
     const body = document.body
 
-    const prevOverflow = body.style.overflow
-    const prevPosition = body.style.position
-    const prevTop = body.style.top
-    const prevWidth = body.style.width
-    const scrollY = window.scrollY
-
     if (block) {
-      body.style.overflow = 'hidden'
+      const scrollY = window.scrollY
       body.style.position = 'fixed'
       body.style.top = `-${scrollY}px`
-      body.style.width = '100%'
-    }
-
-    return () => {
-      body.style.overflow = prevOverflow
-      body.style.position = prevPosition
-      body.style.top = prevTop
-      body.style.width = prevWidth
-      window.scrollTo(0, scrollY)
+      body.style.left = '0'
+      body.style.right = '0'
+      body.style.overflow = 'hidden'
+      body.dataset.scrollY = String(scrollY)
+    } else {
+      const scrollY = body.dataset.scrollY
+      if (scrollY) {
+        body.style.position = ''
+        body.style.top = ''
+        body.style.left = ''
+        body.style.right = ''
+        body.style.overflow = ''
+        window.scrollTo(0, parseInt(scrollY, 10))
+        delete body.dataset.scrollY
+      }
     }
   }, [block])
 }
